9927a1782f1c9e3ea59421f47f8429cd368bca39
larrybah <larrybah@gmail.com>
2023-06-24 10:11:34 +0000

    printf header file

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+#include <limits.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+
+#endif
