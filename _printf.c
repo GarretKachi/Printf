31d88ce3ef06f0309b77955e8b1443eaab4440cb
larrybah <larrybah@gmail.com>
2023-06-24 10:52:01 +0000

    prints formated output to stdout

---
+++ _printf.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _printf - prints formatted output to stdout
+ * @format: length of formatted output
+ * Return: formated output to stdout
+ */
+int _printf(const char *format, ...)
+{
+
+}
