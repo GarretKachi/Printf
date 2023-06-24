63879f43bc55ceaf5d747615dd5595b0bb047f5c
larrybah <larrybah@gmail.com>
2023-06-24 21:51:11 +0000

    prints formated ouput

--- _printf.c
+++ _printf.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
- * _printf - prints formatted output to stdout
- * @format: length of formatted output
- * Return: formated output to stdout
+ * _printf - printfs formated output to stdout
+ * @format: format specifier
+ * Return: printed output
  */
 int _printf(const char *format, ...)
 {
-	int i, j, ch, printed_chars = 0;
-	char *str;
+	int i, printed_chars = 0;
 
 	va_list args;
+
 	va_start(args, format);
 
 	for (i = 0; format[i] != '\0'; i++)
@@ -17,50 +17,14 @@ int _printf(const char *format, ...)
 		if (format[i] == '%')
 		{
 			++i;
-
-			switch (format[i])
-			{
-				case 'c':
-					{
-						ch = va_arg(args, int);
-						_putchar(ch);
-						++printed_chars;
-						break;
-					}
-
-				case 's':
-					{
-						str = va_arg(args, char*);
-						for (j = 0; str[j] != '\0'; j++)
-						{
-							_putchar(str[j]);
-							++printed_chars;
-						}
-						break;
-					}
-				case '%':
-					{
-						_putchar('%');
-						++printed_chars;
-						break;
-					}
-				default:
-					{
-						_putchar('%');
-						_putchar(format[i]);
-						printed_chars += 2;
-						break;
-					}
-			}
+			printed_chars += process_format_specifier(format[i], args);
 		}
-
 		else
 		{
 			_putchar(format[i]);
 			++printed_chars;
 		}
 	}
-
 	va_end(args);
 	return (printed_chars);
 }
---
+++ format_specifiers.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * process_format_specifier - checks cases for formats specifiers
+ * @specifier: format specifier
+ * @args: variable arguments
+ * Return: specifier
+ */
+int process_format_specifier(char specifier, va_list args)
+{
+	int j, ch, printed_chars 0;
+	char *str;
+
+	switch (specifier)
+	{
+		case 'c':
+		{
+			ch = va_args(args, int);
+			_putchar(ch);
+			return (1);
+		}
+		case 's':
+		{
+			str = va_args(args, char*);
+			for (j = 0; str[j] != '\0'; ++j)
+			{
+				_putchar(str[j]);
+				++printed_chars;
+			}
+			return (printed_chars);
+		}
+		case '%':
+		{
+			_putchar('%');
+			return (1);
+		}
+		default:
+		{
+			_putchar('%');
+			_putchar(specifier);
+			return (2);
+		}
+	}
+}
